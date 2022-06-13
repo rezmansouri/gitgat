35accdf7bb0633e7e06a897bb138ebd7c16f7816
Reza Mansouri <mrmansourireza@gmail.com>
2022-06-13 23:03:35 +0430

    initial commit

---
+++ gitgat.py
@@ -0,0 +1,59 @@
+from telegram import Update
+from telegram.ext import Updater, CallbackContext, CommandHandler
+import helpers
+
+# Access Token of your bot acquired from @BotFather
+token = ''
+
+
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Hi I\'m GitGat!\nI can send a subdirectory from a GitHub repository directly to you as zip! try /get along with the URL!")
+
+
+def error(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='I\'m sorry something unexpected happened')
+
+
+def get(update: Update, context: CallbackContext):
+    if len(context.args) == 0:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='You should provide a URL along with the command. like this:')
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='/get https://github.com/rezmansouri/gitgat')
+        return
+    url = context.args[0]
+    dir_name = helpers.validate_url(url)
+    if dir_name is False:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='Please give me a valid GitHub directory/repository URL\nfor example: `https://github.com/rezmansouri/gitgat`\nor: `https://github.com/rust-lang/rust/tree/master/src/test`')
+        return
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Hold on I\'m fetching the files...')
+    helpers.crawl(url, dir_name)
+    helpers.make_archive(base_name=helpers.storage_address + dir_name, format='zip',
+                         root_dir=helpers.storage_address + dir_name)
+    context.bot.send_message(chat_id=update.effective_chat.id, text='I fetched the files! I\'m sending them...')
+    with open(f'{helpers.storage_address}/{dir_name}.zip', 'rb') as output:
+        context.bot.send_document(chat_id=update.effective_chat.id, document=output,
+                                  reply_to_message_id=update.effective_message.message_id)
+    helpers.clear_storage(dir_name)
+
+
+def main():
+    updater = Updater(token=token)
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    get_handler = CommandHandler('get', get)
+    dispatcher.add_handler(get_handler)
+
+    dispatcher.add_error_handler(error)
+
+    updater.start_polling()
+
+
+if __name__ == '__main__':
+    main()
---
+++ helpers.py
@@ -0,0 +1,44 @@
+import os
+import re
+from urllib import request as req, parse
+from shutil import make_archive, rmtree
+from bs4 import BeautifulSoup
+
+github_file_root = 'https://raw.githubusercontent.com'
+github_dir_root = 'https://github.com/'
+file_css_tag = 'js-navigation-open Link--primary'
+storage_address = 'storage/'
+
+
+def crawl(url: str, dir_name):
+    """
+    this is a recursive function.
+    it receives a directory url and looks into the directory.
+    if a directories is found within, it is crawled again, otherwise the file will be downloaded.
+    """
+    os.mkdir(storage_address + dir_name)
+    page = req.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
+    url_elements = soup.find_all('a', class_=file_css_tag)
+    for url_element in url_elements:
+        url_ = url_element.attrs['href']
+        base_name = parse.unquote(os.path.basename(url_))
+        if url_.__contains__('/blob/'):
+            file_url = github_file_root + url_.replace('/blob/', '/')
+            req.urlretrieve(file_url, storage_address + dir_name + '/' + base_name)
+        else:
+            crawl(github_dir_root + url_, f'{dir_name}/{base_name}')
+
+
+def clear_storage(dir_name):
+    os.remove(f'{storage_address}{dir_name}.zip')
+    rmtree(f'{storage_address}{dir_name}')
+
+
+def validate_url(url: str):
+    if not (url.startswith('http://') or url.startswith('https://')):
+        url = 'https://' + url
+    if re.search('https://github.com(/[^/].*){2,}', url) is None:
+        return False
+    dir_name = parse.unquote(parse.unquote(url.split('/')[-1]))
+    return dir_name
