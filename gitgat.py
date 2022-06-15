82acb59e3f27a46b376f73c0e4448d519dcd7bd8
Reza Mansouri <mrmansourireza@gmail.com>
2022-06-15 13:42:42 +0430

    bug fix

--- gitgat.py
+++ gitgat.py
@@ -1,5 +1,7 @@
 from telegram import Update
 from telegram.ext import Updater, CallbackContext, CommandHandler
+from uuid import uuid1
+import logging
 import helpers
 
 # Access Token of your bot acquired from @BotFather
@@ -14,6 +16,7 @@ def start(update: Update, context: CallbackContext):
 def error(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text='I\'m sorry something unexpected happened')
+    print(context.error)
 
 
 def get(update: Update, context: CallbackContext):
@@ -24,20 +27,21 @@ def get(update: Update, context: CallbackContext):
                                  text='/get https://github.com/rezmansouri/gitgat')
         return
     url = context.args[0]
-    url, dir_name = helpers.validate_url(url)
-    if dir_name is False:
+    url, file_name = helpers.validate_url(url)
+    if file_name is False:
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text='Please give me a valid GitHub directory/repository URL\nfor example: https://github.com/rezmansouri/gitgat\nor: https://github.com/rust-lang/rust/tree/master/src/test')
         return
     context.bot.send_message(chat_id=update.effective_chat.id, text='Hold on I\'m fetching the files...')
-    helpers.crawl(url, dir_name)
-    helpers.make_archive(base_name=helpers.storage_address + dir_name, format='zip',
-                         root_dir=helpers.storage_address + dir_name)
+    unique_dir = str(uuid1())
+    helpers.crawl(url, unique_dir)
+    helpers.make_archive(base_name=helpers.storage_address + unique_dir, format='zip',
+                         root_dir=helpers.storage_address + unique_dir)
     context.bot.send_message(chat_id=update.effective_chat.id, text='I fetched the files! I\'m sending them...')
-    with open(f'{helpers.storage_address}/{dir_name}.zip', 'rb') as output:
+    with open(f'{helpers.storage_address}/{unique_dir}.zip', 'rb') as output:
         context.bot.send_document(chat_id=update.effective_chat.id, document=output,
-                                  reply_to_message_id=update.effective_message.message_id)
-    helpers.clear_storage(dir_name)
+                                  reply_to_message_id=update.effective_message.message_id, filename=file_name)
+    helpers.clear_storage(unique_dir)
 
 
 def main():
@@ -51,6 +55,9 @@ def main():
     dispatcher.add_handler(get_handler)
 
     dispatcher.add_error_handler(error)
+
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
 
     updater.start_polling()
 
